835d63293c08f3e70c3cb889f1b17a52e165ce38
GitHub Action <action@github.com>
2026-05-03 16:46:55 +0000

    Auto: Update crypto data

---
+++ src/data_crypto.js
@@ -0,0 +1,545 @@
+module.exports = {
+  "timestamp": "2026-05-03T16:46:51.915Z",
+  "cryptos": {
+    "BTCUSDT": {
+      "symbol": "BTCUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "ETHUSDT": {
+      "symbol": "ETHUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "BNBUSDT": {
+      "symbol": "BNBUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "XRPUSDT": {
+      "symbol": "XRPUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "ADAUSDT": {
+      "symbol": "ADAUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "SOLUSDT": {
+      "symbol": "SOLUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "DOGEUSDT": {
+      "symbol": "DOGEUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "DOTUSDT": {
+      "symbol": "DOTUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "DYDXUSDT": {
+      "symbol": "DYDXUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "AVAXUSDT": {
+      "symbol": "AVAXUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "LINKUSDT": {
+      "symbol": "LINKUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "MATICUSDT": {
+      "symbol": "MATICUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "UNIUSDT": {
+      "symbol": "UNIUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "ATOMUSDT": {
+      "symbol": "ATOMUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "LTCUSDT": {
+      "symbol": "LTCUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "VETUSDT": {
+      "symbol": "VETUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "FILUSDT": {
+      "symbol": "FILUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "AXSUSDT": {
+      "symbol": "AXSUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "CHZUSDT": {
+      "symbol": "CHZUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "EGLDUSDT": {
+      "symbol": "EGLDUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "THETAUSDT": {
+      "symbol": "THETAUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "SANDUSDT": {
+      "symbol": "SANDUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "MANAUSDT": {
+      "symbol": "MANAUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "ZECUSDT": {
+      "symbol": "ZECUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "XMRUSDT": {
+      "symbol": "XMRUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "ENJUSDT": {
+      "symbol": "ENJUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "FLOWUSDT": {
+      "symbol": "FLOWUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "ICXUSDT": {
+      "symbol": "ICXUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "KSMUSDT": {
+      "symbol": "KSMUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "SCRTUSDT": {
+      "symbol": "SCRTUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "ALPHAUSDT": {
+      "symbol": "ALPHAUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "AUDIOUSDT": {
+      "symbol": "AUDIOUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "COTIUSDT": {
+      "symbol": "COTIUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "GHSTUSDT": {
+      "symbol": "GHSTUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "IMXUSDT": {
+      "symbol": "IMXUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "LRCUSDT": {
+      "symbol": "LRCUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "MASKUSDT": {
+      "symbol": "MASKUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "MTLUSDT": {
+      "symbol": "MTLUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "OMGUSDT": {
+      "symbol": "OMGUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "PERLUSDT": {
+      "symbol": "PERLUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "RNDRUSDT": {
+      "symbol": "RNDRUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "SKLUSDT": {
+      "symbol": "SKLUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "SNXUSDT": {
+      "symbol": "SNXUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "STMXUSDT": {
+      "symbol": "STMXUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "STORJUSDT": {
+      "symbol": "STORJUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "SUSHIUSDT": {
+      "symbol": "SUSHIUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "SXPUSDT": {
+      "symbol": "SXPUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "TOMOUSDT": {
+      "symbol": "TOMOUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "TRBUSDT": {
+      "symbol": "TRBUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "TRUUSDT": {
+      "symbol": "TRUUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "USDCUSDT": {
+      "symbol": "USDCUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "DAIUSDT": {
+      "symbol": "DAIUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "TUSDUSDT": {
+      "symbol": "TUSDUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "BUSDUSDT": {
+      "symbol": "BUSDUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "FDUSDUSDT": {
+      "symbol": "FDUSDUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "PAXUSDT": {
+      "symbol": "PAXUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "USDPUSDT": {
+      "symbol": "USDPUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "CUSDUSDT": {
+      "symbol": "CUSDUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "USDDUSDT": {
+      "symbol": "USDDUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "ORSUSDT": {
+      "symbol": "ORSUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "OUSUSDT": {
+      "symbol": "OUSUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "PYUSUSDT": {
+      "symbol": "PYUSUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "AEUSDT": {
+      "symbol": "AEUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "GTUSDT": {
+      "symbol": "GTUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "WBETHUSDT": {
+      "symbol": "WBETHUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "RETHUSDT": {
+      "symbol": "RETHUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "LSETHUSDT": {
+      "symbol": "LSETHUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "STETHUSDT": {
+      "symbol": "STETHUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "XMUSDT": {
+      "symbol": "XMUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "STONUSDT": {
+      "symbol": "STONUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "SHIBUSDT": {
+      "symbol": "SHIBUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "PEPEUSDT": {
+      "symbol": "PEPEUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "DOGSUSDT": {
+      "symbol": "DOGSUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "FLOKIUSDT": {
+      "symbol": "FLOKIUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "BONKUSDT": {
+      "symbol": "BONKUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "WIFUSDT": {
+      "symbol": "WIFUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "SUIUSDT": {
+      "symbol": "SUIUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "APTUSDT": {
+      "symbol": "APTUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "ARBITRUSDT": {
+      "symbol": "ARBITRUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "OPUSDT": {
+      "symbol": "OPUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "MAGICUSDT": {
+      "symbol": "MAGICUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "GFTUSDT": {
+      "symbol": "GFTUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "ONTUSDT": {
+      "symbol": "ONTUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "RNDR": {
+      "symbol": "RNDR",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "JUPUSDT": {
+      "symbol": "JUPUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "PHBUSDT": {
+      "symbol": "PHBUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "AMUSDT": {
+      "symbol": "AMUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "ZKUSDT": {
+      "symbol": "ZKUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "ORDIUSDT": {
+      "symbol": "ORDIUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    },
+    "VEROUSDT": {
+      "symbol": "VEROUSDT",
+      "price": null,
+      "change24h": null,
+      "volume": null
+    }
+  }
+};
